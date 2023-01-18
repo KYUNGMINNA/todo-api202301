b93c02598d2b7d6bae17fa8167e5e89ee707e2c3
KYUNGMINNA <nkm4095@naver.com>
2023-01-18 14:35:44 +0900

    # to-do Entity 생성

---
+++ src/main/java/com/example/todo/todoapi/entity/TodoEntity.java
@@ -0,0 +1,31 @@
+package com.example.todo.todoapi.entity;
+
+//일정관리 프로그램
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode(of = "todoId")
+@Entity
+@Table(name = "tbl_todo")
+public class TodoEntity {
+    //절대 중복이 없는 랜덤 문자열로 pk값이 처리 됨
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid",strategy = "uuid")
+    private String todoId;
+
+    @Column(nullable = false,length = 30)
+    private String title; //제목
+
+    private boolean done; //일정 완료 여부
+
+    @CreationTimestamp
+    private LocalDateTime createDate; //등록 시간
+
+}
