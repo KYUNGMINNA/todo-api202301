a4b0fd5abd53b445b9d8e8ef2e29a37ad9b17da4
KYUNGMINNA <nkm4095@naver.com>
2023-01-20 17:06:48 +0900

    # controller @CrossOrigin

--- src/main/java/com/example/todo/todoapi/controller/TodoApiController.java
+++ src/main/java/com/example/todo/todoapi/controller/TodoApiController.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RequestMapping("/api/todos")
 @RequiredArgsConstructor
+//CORS 허용 설정
+@CrossOrigin // 나중에 배포 할 때에도 클라이언트 앱의 URL을 적어야 함 !! -- @CrossOrigin(origins="")으로 특정 URL만 가능
 public class TodoApiController {
     private final TodoService todoService;
 
