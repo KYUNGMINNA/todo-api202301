59c33c5d3356599d59cf1664c27c8424e9c1ff29
KYUNGMINNA <nkm4095@naver.com>
2023-02-01 15:31:52 +0900

    # JAR 배포설정4

--- src/main/java/com/example/todo/config/CorsConfig.java
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig  implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         //api cors 정책 설정
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") //api 요청 허용 URL
+                .allowedOrigins("http://localhost:3000",
+                        "http://practice-s3-nkm-bucket001.s3-website.ap-northeast-2.amazonaws.com") //api 요청 허용 URL
                 .allowedMethods("GET","POST","PUT","DELETE") //이 4개의 요청 메서드만 허용
                 .allowedHeaders("*") //헤더 제약 없음
                 . allowCredentials(true)
